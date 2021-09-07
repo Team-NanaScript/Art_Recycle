d16caaa11a61be718b82dec4225d48b14d498d33
soyeon-noh <1010qwerty12@gmail.com>
2021-09-07 16:46:44 +0900

    localSearch.pug 생성

--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -6,4 +6,8 @@ router.get("/", function (req, res, next) {
   res.render("search");
 });
 
+router.get("/local", function (req, res, next) {
+  res.render("localSearch");
+});
+
 module.exports = router;
