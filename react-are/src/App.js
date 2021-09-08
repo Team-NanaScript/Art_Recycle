c009b0801a543821eea68a32b5e93217f6db8898
soyeon-noh <1010qwerty12@gmail.com>
2021-09-08 16:45:04 +0900

    React로 프로젝트 전환

--- react-are/src/App.js
+++ react-are/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MainNav from "./comps/MainNav";
+import { Footer, MainBody } from "./comps";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <MainNav />
+        <MainBody />
+        <Route path="/" component={MainBody} exact />
+        <Route path="/login" component={Login} />
+        <Route path="/join" component={Join} />
+        <Route path="/join" component={Join} />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ react-are/src/comps/index.jsx
@@ -0,0 +1,5 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import MainBody from "./MainBody";
+
+export { Header, Footer, MainBody };
