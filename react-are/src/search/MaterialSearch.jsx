9cd96919c7df320880a9e7100da5cf77a1ab3b39
soyeon-noh <1010qwerty12@gmail.com>
2021-09-10 00:49:48 +0900

    search css 수정

--- react-are/src/search/MaterialSearch.jsx
+++ react-are/src/search/MaterialSearch.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useCallback } from "react";
 
+const materialData = [
+  "목재",
+  "직물",
+  "플라스틱",
+  "가죽",
+  "세라믹",
+  "석재",
+  "종이",
+  "금속",
+  "유리",
+  "고무",
+  "기타",
+];
 const MaterialSearch = () => {
+  const material_box = useCallback(() => {
+    return materialData.map((text) => {
+      return <div className="tag">{text}</div>;
+    });
+  }, []);
   return (
-    <div>
-      <h1>재료고르기</h1>
-    </div>
+    <section className="search_section">
+      <div className="material_box">{material_box()}</div>
+    </section>
   );
 };
 
