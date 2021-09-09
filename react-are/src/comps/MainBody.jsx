a4e57771d16d2f13cfcee089b433d832239aa20a
sk421120 <sk421120@naver.com>
2021-09-09 17:08:37 +0900

    layout

---
+++ react-are/src/comps/MainBody.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainBody() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default MainBody;
---
+++ react-are/src/comps/MainNav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainNav() {
+  return (
+    <div>
+      <h1>Nav</h1>
+    </div>
+  );
+}
+
+export default MainNav;
