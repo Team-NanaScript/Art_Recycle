2f7f1eb6d63e98371a6558fef1f43d6ec3b456a7
soyeon-noh <1010qwerty12@gmail.com>
2021-09-14 16:53:03 +0900

    mongo 연동해보기

---
+++ mongo-are/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const user = Schema({
+  u_id: String,
+  u_pw: String,
+  u_email: String,
+  u_name: String,
+  u_nickname: String,
+  u_role: Number,
+});
+
+module.exports = mongoose.model("are_user", user);
