c4f192ee4355e41c6ebb4c4886833b8db1700905
soyeon-noh <1010qwerty12@gmail.com>
2021-09-14 22:14:05 +0900

    mongoDB 연동

--- mongo-are/routes/index.js
+++ mongo-are/routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const USER = require("../models/user");
+
+const userList = {
+  u_id: "nana",
+  u_pw: "nanana",
+  u_email: "nananana",
+  u_name: "na",
+  u_nickname: "naa",
+  u_role: 00,
+};
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/data", (req, res) => {
+  const result = USER.create(userList);
+  res.json(userList);
 });
 
 module.exports = router;
