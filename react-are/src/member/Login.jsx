3217089c04b703c078359169c57f1ae40cdfdc41
Nanask <ybhag1005@daum.net>
2021-09-14 16:57:43 +0900

    join

--- react-are/src/member/Login.jsx
+++ react-are/src/member/Login.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import "../css/Login.css"
-import { Link } from 'react-router-dom' 
+import "../css/Login.css";
+import { Link } from "react-router-dom";
 
 function Login() {
-
   return (
     <div>
-	<header>
-	{/* <div id="logo">
+      <header>
+        {/* <div id="logo">
         <svg width="97" height="35">
           <text transform="translate(0 29)" fill="#27187f" fontSize="35" fontFamily="Ubuntu-Bold, Ubuntu" fontWeight="700">
             <tspan x="0" y="0">Art</tspan>
@@ -15,26 +14,26 @@ function Login() {
           </text>
 		  </svg>
 		 </div>    */}
-	</header>
-	<div className="id_pw">
-      <div className="input_id">
-		  <input name="u_id" placeholder="ID" />
-		  {/* <span class="far fa-user"></span> */}
-	  </div>
-	  <div className="input_id">
-		<input name="u_pw" placeholder="PW" />
-		{/* <span class="fas fa-unlock-alt"></span> */}
-	  </div>
-	  <Link to="..comps/MainBody">
-	  <button className="btn_login">LOGIN</button>
-	  </Link>
-	  <div className="btn_button">
-		<button className="btn_find">ID/PW 찾기</button>
-		<Link to="./Join">
-		<button className="btn_join">회원가입</button>
-		</Link>
-	  </div>
-	  </div>
+      </header>
+      <div className="id_pw">
+        <div className="input_id">
+          <input name="u_id" placeholder="ID" />
+          <span class="far fa-user"></span>
+        </div>
+        <div className="input_id">
+          <input name="u_pw" placeholder="PW" />
+          <span class="fas fa-unlock-alt"></span>
+        </div>
+        <Link to="/">
+          <button className="btn_login">LOGIN</button>
+        </Link>
+        <div className="btn_button">
+          <button className="btn_find">ID/PW 찾기</button>
+          <Link to="./Join">
+            <button className="btn_join">회원가입</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
