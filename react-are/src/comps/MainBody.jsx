6a0ae49254587d0f90f12547561dca5746e9c77e
박은빈 <sk421120@naver.com>
2021-09-18 22:43:24 +0900

    main_search_query

--- react-are/src/comps/MainBody.jsx
+++ react-are/src/comps/MainBody.jsx
@@ -2,17 +2,30 @@ import React from "react";
 import "../css/main.css";
 
 function MainBody() {
+  const keyToSearch = (e)=>{
+    if(e.keyCode === 13) {
+      let search_query = e.target.value
+      alert(search_query)
+    }
+  }
+
+  const btnToSearch = ()=>{
+      let search_query = document.querySelector("input.main_search").value
+      alert(search_query)
+    
+  }
   return (
     <section className="main_body">
       <article className="main_top">
         <h1>국내 재활용 공방을 한곳에</h1>
         <div className="main_search_box">
           <input
+          onKeyDown={keyToSearch}
             name="search"
             className="main_search"
             placeholder="공방명이나 재료를 입력해주세요"
           />
-          <button>
+          <button onClick={btnToSearch}>
             <span class="fas fa-search fa-2x"></span>
           </button>
         </div>
@@ -24,18 +37,15 @@ function MainBody() {
           <span class="fas fa-map-marked-alt fa-4x"></span>
         </div>
         <div className="material">
-          {/* <h2>직접 가기</h2>
-          <p>직접 찾아가 재활용품을 전달하거나, 제작에 참여 할 수 있어요</p> */}
           <h2>재료별 검색</h2>
           <p>새로 태어날 재활용품 재료들을 찾아볼 수 있어요 </p>
-          <span class="fas fa-walking fa-4x"></span>
+          <span class="far fa-lightbulb fa-4x"></span>
         </div>
         <div className="way">
-          {/* 알아서 제목 바꿔주세욤 */}
           <h2>택배 / 방문</h2>
           <p>택배를 보내거나 직접 방문하여 재활용품을 전달할 수 있어요</p>
-          {/* <span class="fas fa-gift fa-4x"></span> */}
-          <span class="fas fa-box fa-4x"></span>
+          {/* <span class="fas fa-box fa-4x"></span> */}
+          <span class="fas fa-seedling fa-4x"></span>
         </div>
       </article>
     </section>
