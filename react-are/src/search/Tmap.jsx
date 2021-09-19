a5b7ec5fedea677d4d87b2cb50759cd2568b6e82
kimyoungjin98 <gyu250@naver.com>
2021-09-20 02:06:37 +0900

    지도..

--- react-are/src/search/Tmap.jsx
+++ react-are/src/search/Tmap.jsx
@@ -75,9 +75,54 @@ function TMap() {
 		
 		///// 좌표 /////
 
+
          }
          
          initTmap();
+
+		 function loadGetAddressFromLonLat() {
+			// TData 객체 생성
+			var tData = new Tmapv2.extension.TData();
+	
+			var optionObj = {
+				coordType: "WGS84GEO"       //응답좌표 타입 옵션 설정 입니다.
+			};
+	
+			var params = {
+				onComplete:onComplete,      //데이터 로드가 성공적으로 완료 되었을때 실행하는 함수 입니다.
+				onProgress:onProgress,      //데이터 로드 중에 실행하는 함수 입니다.
+				onError:onError             //데이터 로드가 실패했을때 실행하는 함수 입니다.
+			};
+			
+			// TData 객체의 지오코딩 함수
+			tData.getGeoFromAddressJson("서울","은평","갈현","397", optionObj, params);
+		}
+
+		function onComplete() {
+			console.log(this._responseData); //json으로 데이터를 받은 정보들을 콘솔창에서 확인할 수 있습니다.
+	
+			var lon = this._responseData.coordinateInfo.lon;//json으로 데이터를 받은 정보에서 경도정보를 추출합니다.
+			var lat = this._responseData.coordinateInfo.lat;//json으로 데이터를 받은 정보에서 위도정보를 추출합니다.
+	
+			var marker = new Tmapv2.Marker({
+				position : new Tmapv2.LatLng(lat, lon)
+			});
+	
+			marker.setMap(map);
+			map.setCenter(new Tmapv2.LatLng(lat, lon));
+		}
+		
+		//데이터 로드중 실행하는 함수입니다.
+		function onProgress(){
+			
+		}
+		
+		  //데이터 로드 중 에러가 발생시 실행하는 함수입니다.
+		function onError(){
+			alert("onError");
+		}
+
+		loadGetAddressFromLonLat()
       `;
     script.type = "text/javascript";
     script.async = "async";
