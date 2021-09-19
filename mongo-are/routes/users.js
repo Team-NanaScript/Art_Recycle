dbf7df956128938d50fa89b56a6d7f056b38e8d8
박은빈 <sk421120@naver.com>
2021-09-19 23:48:38 +0900

    Main_search

--- mongo-are/routes/users.js
+++ mongo-are/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const userList = {
+  u_id: "nana",
+  u_pw: "nanana",
+  u_email: "nananana",
+  u_name: "na",
+  u_nickname: "naa",
+  u_role: 00,
+};
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/test', (req, res)=> {
+  res.json(userList);
+});
+
+router.get('/test/:query', (req, res)=> {
+  let query = req.params.query;
+  console.log(query);
+  const tmpUserList = {...userList, u_nickname:query};
+  // userList = [...userList, u_nickname=query]
+  res.json(tmpUserList);
 });
 
 module.exports = router;
--- react-are/src/comps/MainBody.jsx
+++ react-are/src/comps/MainBody.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import "../css/main.css";
+import { useHistory } from "react-router-dom"; 
 
-function MainBody() {
+function MainBody( props ) {
+  const searchList = props.searchList;
+  const history = useHistory();
+
+  // const userFetch = useCallback(async  ()=>{
+  //   const res = await fetch("http://localhost:5000/users/test")
+  //   const userList = await res.json();
+  //   await setUserList(userList);
+  // }, []);
+
+  // useEffect(userFetch, [userFetch]);
+
   const keyToSearch = (e)=>{
     if(e.keyCode === 13) {
       let search_query = e.target.value
-      alert(search_query)
+      // alert(search_query)
+      const tmpList = searchList(search_query);
+      // history.push(`/search/result/${search_query}`, tmpList)
+      history.push(`/search/${search_query}`)
     }
   }
 
@@ -18,6 +33,7 @@ function MainBody() {
     <section className="main_body">
       <article className="main_top">
         <h1>국내 재활용 공방을 한곳에</h1>
+        
         <div className="main_search_box">
           <input
           onKeyDown={keyToSearch}
@@ -34,18 +50,18 @@ function MainBody() {
         <div className="area">
           <h2>지역별 공방</h2>
           <p>우리 지역의 공방을 찾아봐요</p>
-          <span class="fas fa-map-marked-alt fa-4x"></span>
+          <span class="fas fa-map-marked-alt fa-4x up"></span>
         </div>
         <div className="material">
           <h2>재료별 검색</h2>
           <p>새로 태어날 재활용품 재료들을 찾아볼 수 있어요 </p>
-          <span class="far fa-lightbulb fa-4x"></span>
+          <span class="far fa-lightbulb fa-4x up"></span>
         </div>
         <div className="way">
           <h2>택배 / 방문</h2>
           <p>택배를 보내거나 직접 방문하여 재활용품을 전달할 수 있어요</p>
           {/* <span class="fas fa-box fa-4x"></span> */}
-          <span class="fas fa-seedling fa-4x"></span>
+          <span class="fas fa-seedling fa-4x up"></span>
         </div>
       </article>
     </section>
