c65fb97f86bf9b8d9601b8b73c6fdf389dac6256
soyeon-noh <1010qwerty12@gmail.com>
2021-09-20 23:36:17 +0900

    Search에 HashTag state 추가, state 변경함수 선언

--- react-are/src/search/Search.jsx
+++ react-are/src/search/Search.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/SearchSection.css";
 
 import { BrowserRouter, Route } from "react-router-dom";
@@ -11,6 +11,20 @@ import {
 } from "../search";
 
 const Search = () => {
+  const [hashTag, setHashTag] = useState([]);
+
+  const hashTagChange = (event) => {
+    // const { name, value } = event.target;
+
+    const _tag = [...hashTag];
+
+    _tag[_tag.length] = "다진마늘";
+    setHashTag(_tag);
+
+    _tag[_tag.length] = "갈매기";
+    setHashTag(_tag);
+  };
+
   return (
     <BrowserRouter>
       <div className="Search">
@@ -18,9 +32,24 @@ const Search = () => {
           <h2>공방찾기</h2>
         </header>
         <SearchNav />
-        <Route path="/search/local" component={LocalSearch} exact />
-        <Route path="/search/way" component={WaySearch} exact />
-        <Route path="/search/material" component={MaterialSearch} exact />
+        <Route
+          path="/search/local"
+          component={LocalSearch}
+          hashTagChange={hashTagChange}
+          exact
+        />
+        <Route
+          path="/search/way"
+          component={WaySearch}
+          hashTagChange={hashTagChange}
+          exact
+        />
+        <Route
+          path="/search/material"
+          component={MaterialSearch}
+          hashTagChange={hashTagChange}
+          exact
+        />
         <div className="result_tag"></div>
         <Result />
       </div>
