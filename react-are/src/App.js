134e77e481b75b6faa0ff6ac5813c35139eb6465
박은빈 <sk421120@naver.com>
2021-09-25 18:56:12 +0900

    tagAddTry1

--- react-are/src/App.js
+++ react-are/src/App.js
@@ -11,19 +11,39 @@ function App() {
   const [hashTag, setHashTag] = useState([]);
 
   const changeTag = (tagText) => {
-    const _tag = [...hashTag];
+    // const _tag = [...hashTag];
+    // const _tag = [...tagList];
 
-    alert("_tag길이: " + _tag.length); // 길이가 계속 0인 문제 발생
+    // alert("_tag길이: " + _tag.length); // 길이가 계속 0인 문제 발생
     // _tag[_tag.length] = tagText;
-    _tag.push(tagText);
+    // _tag.push(tagText);
 
-    setHashTag(_tag);
-    alert("_tag길이: " + _tag.length); // 길이가 계속 1인 문제 발생
-    alert("hashTag 길이: " + hashTag.length); // 길이가 계속 0인 문제 발생
+    // setHashTag(_tag);
+    // alert("tag : "+ _tag[0] + " hash : " + tagList[0])
+    // alert("_tag길이: " + _tag.length); // 길이가 계속 1인 문제 발생
+    // alert("hashTag 길이: " + tagList.length); // 길이가 계속 0인 문제 발생
     // alert("tagText: " + tagText); //정상
-    console.table(hashTag); // undefined 로만 뜬다.
+    // console.table(tagList); // undefined 로만 뜬다.
+    tmp_changeTag(tagText, hashTag, setHashTag)
   };
-  
+
+  const tmp_changeTag = (tagText, hashTag, setHashTag) =>{
+    const _tag = [...hashTag];
+    // alert("tag : "+ _tag[0] + " hash : " + hashTag[0])
+    _tag.push(tagText);
+    setHashTag(_tag);
+    alert("tag : "+ _tag[0] + " hash : " + hashTag[0])
+  }
+
+  const args = {
+    changeTag,
+    hashTag,
+    setHashTag
+  }
+
+  // const args_result = {
+   
+  // }
   return (
     <BrowserRouter>
       <div className="App">
