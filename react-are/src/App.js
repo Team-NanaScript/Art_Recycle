2b4ac2accf58b39e0f80b65a95f32bc5a88d806f
박은빈 <sk421120@naver.com>
2021-09-25 16:13:09 +0900

    useState수정

--- react-are/src/App.js
+++ react-are/src/App.js
@@ -5,20 +5,37 @@ import { BrowserRouter, Route } from "react-router-dom";
 import { Login, Join } from "./member";
 import { Search, Detail} from "./search";
 import ResultTmp from "./search/ResultTmp"
+import { useState } from "react";
 
 function App() {
+  const [hashTag, setHashTag] = useState([]);
+
+  const changeTag = (tagText) => {
+    const _tag = [...hashTag];
+
+    alert("_tag길이: " + _tag.length); // 길이가 계속 0인 문제 발생
+    // _tag[_tag.length] = tagText;
+    _tag.push(tagText);
+
+    setHashTag(_tag);
+    alert("_tag길이: " + _tag.length); // 길이가 계속 1인 문제 발생
+    alert("hashTag 길이: " + hashTag.length); // 길이가 계속 0인 문제 발생
+    // alert("tagText: " + tagText); //정상
+    console.table(hashTag); // undefined 로만 뜬다.
+  };
   
   return (
     <BrowserRouter>
       <div className="App">
         <MainNav />
-        {/* <Route path="/" component={()=><MainBody searchList={searchList} />} exact /> */}
         <Route path="/" component={MainBody} exact />
         <Route path="/login" component={Login} exact/>
         <Route path="/join" component={Join} />
-        <Route path="/search/local" component={Search} exact />
+        <Route path="/search/local" render={() => <Search changeTag={changeTag} />} exact /> 
         <Route path="/search/result/:query" component={ResultTmp} exact/>
         <Route path="/detail" component={Detail} />
+        <Route path="/search/way" component={Search} exact/>
+        <Route path="/search/material" component={Search}/>
         <Footer />
       </div>
     </BrowserRouter>
