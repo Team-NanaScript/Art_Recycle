eb704544875510a16fe3c25ca515c18d67d6551a
Eungyeol41 <iniziato@naver.com>
2021-09-28 17:44:05 +0900

    community 살짝 추가

--- react-are/src/community/Board.jsx
+++ react-are/src/community/Board.jsx
@@ -1,24 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Board() {
-  const [commuList, setCommuList] = useState([]);
+function Board({ commuList }) {
   const commu_body = commuList.map((community) => {
     return (
-      /*
       <tr>
         <td>{community.c_no}</td>
         <td>{community.c_title}</td>
         <td>{community.c_writer}</td>
         <td>{community.c_date}</td>
-        <td>{community.c_count}</td>
-      </tr>
-	  */
-      <tr>
-        <td>1</td>
-        <td>제목</td>
-        <td>작성자</td>
-        <td>날짜</td>
-        <td>조회 수</td>
+        <td>0</td>
       </tr>
     );
   });
@@ -34,17 +24,11 @@ function Board() {
             <th>조회 수</th>
           </tr>
         </thead>
-        {/* <tbody>{commu_body}</tbody> */}
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>제목</td>
-            <td>작성자</td>
-            <td>날짜</td>
-            <td>조회 수</td>
-          </tr>
-        </tbody>
+        <tbody>{commu_body}</tbody>
       </table>
+      <div>
+        <button>글쓰기</button>
+      </div>
     </div>
   );
 }
