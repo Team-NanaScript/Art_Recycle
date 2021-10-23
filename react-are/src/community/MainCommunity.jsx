fe8bf53a3008908d6400e3bfeaea0b7b84f5505c
Eungyeol41 <iniziato@naver.com>
2021-10-23 13:08:44 +0900

    state setting...

--- react-are/src/community/MainCommunity.jsx
+++ react-are/src/community/MainCommunity.jsx
@@ -7,16 +7,6 @@ import Insert from "./Insert";
 
 function MainCommunity() {
   return (
-    // <div>
-    //   <Insert board={board} setBoard={setBoard} commuList={commuList} setCommuList={setCommuList} />
-    //   <Board commuList={commuList} />
-    // </div>
-    // <BrowserRouter>
-    //   <div className="com_main">
-    //     <Route path="/board" component={Board} exact />
-    //     <Route path="/board/insert" component={Insert} />
-    //   </div>
-    // </BrowserRouter>
     <CommunityContextProvider>
       <Route path="/" exact>
         Main
