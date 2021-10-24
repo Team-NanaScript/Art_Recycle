3927bd83a450019236be7db090917fdc3028f280
kimyoungjin98 <gyu250@naver.com>
2021-10-25 00:59:18 +0900

    노트 수정

--- react-are/src/atelier/AteInsert.jsx
+++ react-are/src/atelier/AteInsert.jsx
@@ -2,15 +2,24 @@ import React, { useEffect } from "react";
 // import { CKEditor } from "@ckeditor/ckeditor5-react";
 // import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import "../css/AteInsert.css";
-import note from "../modules/note.js";
 
 const AteInsert = () => {
-  useEffect(
-    () => ({
-      note,
-    }),
-    []
-  );
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.innerHTML = ` 
+			$(document).ready(function(){
+				$('#summernote').summernote({
+					lang: 'ko-KR',
+					height: 300,                 // 에디터 높이
+					minHeight: null,             // 최소 높이
+					maxHeight: null,
+				});
+			})
+				  `;
+    script.type = "text/javascript";
+    script.async = "async";
+    document.head.appendChild(script);
+  }, []);
 
   return (
     <section className="ate_section">
--- react-are/src/community/BoardInsert.jsx
+++ react-are/src/community/BoardInsert.jsx
@@ -6,9 +6,20 @@ function BoardInsert() {
   const { changeInput, onClickSave } = useCommuContext();
 
   useEffect(() => {
-    {
-      note;
-    }
+    const script = document.createElement("script");
+    script.innerHTML = ` 
+			$(document).ready(function(){
+				$('#summernote').summernote({
+					lang: 'ko-KR',
+					height: 300,                 // 에디터 높이
+					minHeight: null,             // 최소 높이
+					maxHeight: null,
+				});
+			})
+				  `;
+    script.type = "text/javascript";
+    script.async = "async";
+    document.head.appendChild(script);
   }, []);
 
   return (
