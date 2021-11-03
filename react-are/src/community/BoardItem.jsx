de4200fea677ab3853e66fae41d4ec57e0be71f2
Eungyeol41 <iniziato@naver.com>
2021-11-03 17:58:17 +0900

    ContextProvider 수정

--- react-are/src/community/BoardItem.jsx
+++ react-are/src/community/BoardItem.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { useHistory } from "react-router";
 import { useCommuContext } from "../context/CommunityContextProvider";
 
 function BoardItem({ board, index }) {
-  const { onTrClick } = useCommuContext();
+  const history = useHistory();
+
+  // 커뮤니티 리스트 -> 디테일
+  const onTrClick = async (e) => {
+    const b_seq = e.target.closest("tr").dataset.id;
+
+    history.replace(`/board/detail/${b_seq}`);
+  };
+
   return (
     <tr onClick={onTrClick} data-id={board.b_seq}>
       <td>{index + 1}</td>
