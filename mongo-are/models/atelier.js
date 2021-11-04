3b6d83d3df00f7c71786d91fd91e40361c063de8
soyeon-noh <1010qwerty12@gmail.com>
2021-11-04 17:46:39 +0900

    atelier insert

--- mongo-are/models/atelier.js
+++ mongo-are/models/atelier.js
@@ -7,9 +7,12 @@ const atelier = Schema({
   at_name: String, // 공방명
   at_subname: String, // 간단소개
   at_addr: String, // 주소
-  at_on: String, // 영업시간
+  at_open: String, // 영업시간
   at_site: String, // 웹사이트
-  at_desc: String, // 공방소개
+  at_hashtag: Array, // 해시태그
+  at_thumbnail: String, // 썸네일
+  at_image: Array, // 이미지파일들
+  at_content: String, // 공방소개
 });
 
 export default mongoose.model("atelier", atelier);
